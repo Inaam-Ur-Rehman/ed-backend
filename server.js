e4fe263c0713a1a0a82c28ebf2152b57877e1e6d
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2022-09-25 10:05:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
+  console.log("connected");
   socket.on("get-document", (documentId) => {
     const data = "";
     socket.join(documentId);
