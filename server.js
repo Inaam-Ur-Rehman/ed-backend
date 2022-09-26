3325b3b41e91d659b1c418f5e20fba02539461bc
Inam Ur Rehman <inaamurrehman343@gmail.com>
2022-09-25 21:10:10 -0700

    Add *

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   // configure websocket transport
   cors: {
-    origin: "https://ed-frontend-iota.vercel.app",
+    origin: "*",
     methods: ["GET", "POST"],
   },
   allowEIO3: true,
